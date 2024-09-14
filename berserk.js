80682de834490872be40efca0eb9dc76ca87e7c6
Ragehunter <119442319+Ragedusk@users.noreply.github.com>
2024-09-14 21:58:32 +0300

    added new feature: never berserk
    added never berserk feature for the users that does not want to consider clicking berserk button/icon on lichess.org

--- berserk.js
+++ berserk.js
@@ -6,76 +6,79 @@
 // @include  /^https:\/\/lichess\.org\/([a-zA-Z0-9\/]){6,}
 // ==/UserScript==
 
-setTimeout(function()
-{
+setTimeout(function () {
     var berserkAlwaysDiv;
     var berserkBackDiv;
+    var berserkNeverDiv;
 
     var berserkAlwaysCheckbox;
     var berserkBackCheckbox;
-
+    var berserkNeverCheckbox;
+    
     var zerked = false; // makes a few things more readable
     var cName; // stores the class of the tournament-defining component (tournament-lobby or tournament-game), in order to add the elements to the correct div
 
 
     // storage functions. remember berserking preferences between games
-    function getStorage(v)
-    {
+    function getStorage(v) {
         return localStorage[v] === 'true';
     }
 
-    function setStorage(v, val)
-    {
+    function setStorage(v, val) {
         localStorage[v] = val;
     }
 
 
-    function clickZerkLoop()
-    {
-        if (document.getElementsByClassName("fbt go-berserk")[0] != undefined && !zerked)
-        {
+    function clickZerkLoop() {
+        if (document.getElementsByClassName("fbt go-berserk")[0] != undefined && !zerked) {
             document.getElementsByClassName("fbt go-berserk")[0].click();
             zerked = true;
         }
-        if (!zerked)
-        {
+        if (!zerked) {
             setTimeout(clickZerkLoop, 50);
         }
     }
 
-    function backZerkLoop()
-    {
-        if (document.getElementsByClassName("rclock rclock-top")[0].getElementsByClassName("berserked top")[0] != undefined)
-        {
+    function backZerkLoop() {
+        if (document.getElementsByClassName("rclock rclock-top")[0].getElementsByClassName("berserked top")[0] != undefined) {
             clickZerkLoop();
         }
-        if (!zerked && berserkBackCheckbox.checked)
-        {
+        if (!zerked && berserkBackCheckbox.checked) {
             setTimeout(backZerkLoop, 150);
+        }
+    }
+
+    // Hides the berserk button, and prevents berserk-always / berserk-back
+    function disableBerserkLoop() {
+        if (berserkNeverCheckbox.checked) {
+            document.getElementsByClassName("fbt go-berserk")[0].style.display = 'none'; // ! hides the berserk icon
+            berserkAlwaysCheckbox.checked = false;
+            berserkBackCheckbox.checked = false;
+            setTimeout(disableBerserkLoop, 150);
         }
     }
 
 
     // looks at which boxes are checked and performs actions
-    function checkBoxes()
-    {
+    function checkBoxes() {
         if (!zerked && document.getElementsByClassName("game__tournament").length > 0) // only try to click the buttons if you are actually playing (not in lobby or observing a tournament game)
         {
-            if (berserkAlwaysCheckbox.checked)
-            {
+            if (!berserkNeverCheckbox.checked && berserkAlwaysCheckbox.checked) {
                 clickZerkLoop();
             }
-            if (berserkBackCheckbox.checked)
-            {
+            if (!berserkNeverCheckbox.checked && berserkBackCheckbox.checked) {
                 backZerkLoop();
             }
+            if (berserkNeverCheckbox.checked){
+                disableBerserkLoop();
+            }
+
         }
     }
 
 
     // checks if page is a tournament lobby or tournament game
-    if (document.getElementsByClassName("tour__meta").length > 0 || document.getElementsByClassName("game__tournament-link").length > 0 || document.getElementsByClassName("game__tournament").length > 0)
-    {
+    if (document.getElementsByClassName("tour__meta").length > 0 || document.getElementsByClassName("game__tournament-link").length > 0 || document.getElementsByClassName("game__tournament").length > 0) {
         cName = (document.getElementsByClassName("tour__meta").length > 0) ? "tour__meta" : "game__meta";
 
         // cosmetics and buttons. div that will contain the new elements
@@ -90,6 +93,11 @@ setTimeout(function()
         berserkBackCheckbox.id = "berserkBackCheckbox";
         berserkBackCheckbox.type = "checkbox";
 
+        // ! trying new feature, never berserk
+        berserkNeverCheckbox = document.createElement("input");
+        berserkNeverCheckbox.id = "berserkNeverCheckbox";
+        berserkNeverCheckbox.type = "checkbox";
+
         // defines the divs that will contain the checkboxes and adds them
         berserkAlwaysDiv = document.createElement("div");
         berserkAlwaysDiv.id = "berserk-always";
@@ -101,25 +109,36 @@ setTimeout(function()
         berserkBackDiv.appendChild(berserkBackCheckbox);
         berserkBackDiv.appendChild(document.createTextNode(" Berserk back"));
 
+        // for people that doesn't want to see/click berserk icon
+        berserkNeverDiv = document.createElement("div");
+        berserkNeverDiv.id = "berserk-never";
+        berserkNeverDiv.appendChild(berserkNeverCheckbox);
+        berserkNeverDiv.appendChild(document.createTextNode(" Never berserk"));
+
         // after everything is setup, add things to the div
         theDiv.appendChild(berserkAlwaysDiv);
         theDiv.appendChild(berserkBackDiv);
+        theDiv.appendChild(berserkNeverDiv);
 
         // adds listeners to checkboxes
-        berserkAlwaysCheckbox.addEventListener("click", function()
-        {
+        berserkAlwaysCheckbox.addEventListener("click", function () {
             setStorage("berserkAlwaysCheckbox", this.checked);
             checkBoxes();
         });
 
-        berserkBackCheckbox.addEventListener("click", function()
-        {
+        berserkBackCheckbox.addEventListener("click", function () {
             setStorage("berserkBackCheckbox", this.checked);
+            checkBoxes();
+        });
+
+        berserkNeverCheckbox.addEventListener("click", function () {
+            setStorage("berserkNeverCheckbox", this.checked);
             checkBoxes();
         });
 
         berserkAlwaysCheckbox.checked = getStorage("berserkAlwaysCheckbox");
         berserkBackCheckbox.checked = getStorage("berserkBackCheckbox");
+        berserkNeverCheckbox.checked = getStorage("berserkNeverCheckbox");
 
         checkBoxes();
         console.log("loaded berserk script");
